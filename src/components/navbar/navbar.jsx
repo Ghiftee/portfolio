1ed0937b6c9ee200dca50ac13758512bd402af70
Ghiftee <gigibetine@gmail.com>
2022-02-02 08:15:30 +0100

    Add ids to components and a bit more styling

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,14 @@ import './navbar.scss';
 
 export default function Navbar() {
   return (
-    <div className="navbar" />
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">GIGI</a>
+        </div>
+        <div className="right">cccc</div>
+
+      </div>
+    </div>
   );
 }
--- src/components/porfolio/portfolio.jsx
+++ src/components/porfolio/portfolio.jsx
@@ -2,7 +2,7 @@ import './portfolio.scss';
 
 export default function Portfolio() {
   return (
-    <div className="portfolio">
+    <div className="portfolio" id="portfolio">
       This is the portfolio section
     </div>
   );
