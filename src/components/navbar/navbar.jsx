3788b241c8dc3961fab6ede748628a0a3a5525b0
Ghiftee <gigibetine@gmail.com>
2022-02-02 09:46:40 +0100

    Complete nav bar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import './navbar.scss';
-import { Person, Mail } from '@material-ui/icons';
+// import { Person, Mail } from '@material-ui/icons';
 
 export default function Navbar() {
   return (
@@ -7,18 +7,24 @@ export default function Navbar() {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">GIGI</a>
-          <div className="itemContainer">
-            <Person className="icon" />
-            <span>+0000000</span>
-          </div>
-          <div className="itemContainer">
-            <Mail className="icon" />
-            <span>gigibetine@gmail.com</span>
-          </div>
         </div>
-        <div className="right">This is right</div>
-
+        <div className="right">
+          <a href="#intro" className="hamburger">Home</a>
+          <a href="#portfolio" className="hamburger">Portfolio</a>
+          <a href="#testimonials" className="hamburger">Testimonials</a>
+          <a href="#contact" className="hamburger">Contact</a>
+        </div>
       </div>
     </div>
   );
 }
+
+// Removed from under intro
+// <div className="itemContainer">
+//             <Person className="icon" />
+//             <span>+0000000</span>
+//           </div>
+//           <div className="itemContainer">
+//             <Mail className="icon" />
+//             <span>gigibetine@gmail.com</span>
+//           </div>
